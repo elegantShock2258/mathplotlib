6e80ffc0ce0df65e9fd2268bab1601d930708f26
Ayush Chadha <elegantshock3307@gmail.com>
2025-07-27 23:07:41 +0530

    work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mathplootIib',  # your package name
-    version='0.53',
+    version='0.54',
     description='hi',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
